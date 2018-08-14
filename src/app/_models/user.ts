7bac84314f8fa152a1a42b696c6391d05f02952e
subbaraoyanamala <yanamala05@gmail.com>
2018-08-14 13:03:41 +0530

    my full registration form

---
+++ src/app/_models/user.ts
@@ -0,0 +1,15 @@
+﻿export class User {
+    id: number;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    dateofbirth: string;
+    salary: number;
+    designation: string;
+    department: string;
+    city: string;
+    state: string;
+    country: string;
+    postalcode: string;
+}
